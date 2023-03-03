c4c7056eba182308d9b9fe3434e782428a409fc7
Rajat <rajatprajapati09@gmail.com>
2023-03-03 19:47:07 +0530

    sort arry

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] a = { 1, 2, 10, 50, 5 };
+
+int temp = 0;
+
+for(int i=0; i<a.Length; i++) 
+{
+  for(int j = i + 1; j < a.Length; j++) 
+  {
+
+        if (a[i] > a[j]) 
+        {
+            temp = a[j];
+            a[j] = a[i];
+            a[i] = temp;
+        }
+  }
+}
+foreach(int i in a) 
+{
+    Console.WriteLine(i);
+}
